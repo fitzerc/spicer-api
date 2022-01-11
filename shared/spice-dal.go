fac290b89867efd245256753867410f46d7bee5a
fitzerc <fitzer.c@gmail.com>
2022-01-10 22:11:26 -0600

    adding file io for persisting data

--- shared/spice-dal.go
+++ shared/spice-dal.go
@@ -1,27 +1,55 @@
 package shared
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type SpiceDal interface {
 	ReadSpices() []Spice
 	WriteSpice(Spice)
 }
 
+//SpiceDal implementation for file IO
 type FsSpiceDal struct {
 }
 
 func (fsDal FsSpiceDal) ReadSpices() []Spice {
-	//seed data
-	var spices = []Spice{
-		{Id: 1, Name: "Oregano", Level: 50, Substitute: "Rosemary"},
-		{Id: 2, Name: "Rosemary", Level: 25, Substitute: "Oregano"},
-		{Id: 3, Name: "Sage", Level: 75, Substitute: "Thyme"},
-		{Id: 4, Name: "Thyme", Level: 125, Substitute: "Sage"},
+	var spices []Spice
+
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, file := range files {
+		if strings.Contains(file.Name(), ".json") {
+			var spice Spice
+
+			data, err := ioutil.ReadFile(file.Name())
+			if err != nil {
+				fmt.Printf("Error reading file: %v\n", err)
+			}
+
+			err = json.Unmarshal([]byte(data), &spice)
+			if err != nil {
+				fmt.Printf("Error converting from %v to spice: %v\n", data, err)
+			}
+
+			spices = append(spices, spice)
+		}
 	}
 
 	return spices
 }
 
 func (fsDal FsSpiceDal) WriteSpice(s Spice) {
-	fmt.Println(s)
+	file, _ := json.MarshalIndent(s, "", " ")
+
+	err := ioutil.WriteFile(s.Name+".json", file, 0644)
+	if err != nil {
+		fmt.Printf("Error writing spice: %v\n", s)
+	}
 }
